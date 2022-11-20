9bf5704f2364177d4f492d902f50ef707e19e4f0
el-mo9awin1954dz <118679598+el-mo9awin1954dz@users.noreply.github.com>
2022-11-20 18:09:24 +0100

    Add files via upload

---
+++ hoster.js
@@ -0,0 +1,65 @@
+//crawl
+//get url
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const args = process.argv.slice(2);
+
+
+const HOST = args[0];
+
+//Main
+if(HOST === undefined) {
+     console.log('\n\n','\x1b[31m',
+     `
+    ⠀⠀⢀⣀⣀⣀⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⢀⠀⠈⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⠀⠁⠀⠘⠁⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠈⠀⠀⡇⠀⠀⠀⠀
+⣀⣀⣀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠄⠀⠀⠸⢰⡏⠉⠳⣄⠰⠀⠀⢰⣷⠶⠛⣧⠀
+⢻⡀⠈⠙⠲⡄⣿⠀⠀⠀⠀⠀⠀⠀⠠⠀⢸⠀⠀⠀⠈⠓⠒⠒⠛⠁⠀⠀⣿⠀
+⠀⠻⣄⠀⠀⠙⣿⠀⠀⠀⠈⠁⠀⢠⠄⣰⠟⠀⢀⡔⢠⠀⠀⠀⠀⣠⠠⡄⠘⢧
+⠀⠀⠈⠛⢦⣀⣿⠀⠀⢠⡆⠀⠀⠈⠀⣯⠀⠀⠈⠛⠛⠀⠠⢦⠄⠙⠛⠃⠀⢸
+⠀⠀⠀⠀⠀⠉⣿⠀⠀⠀⢠⠀⠀⢠⠀⠹⣆⠀⠀⠀⠢⢤⠠⠞⠤⡠⠄⠀⢀⡾
+⠀⠀⠀⠀⠀⢀⡿⠦⢤⣤⣤⣤⣤⣤⣤⣤⡼⣷⠶⠤⢤⣤⣤⡤⢤⡤⠶⠖⠋⠀
+⠀⠀⠀⠀⠀⠸⣤⡴⠋⠸⣇⣠⠼⠁⠀⠀⠀⠹⣄⣠⠞⠀⢾⡀⣠⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀
+    `,'\x1b[0m');
+    console.log('DZ-HACKING-LAB @EL-MO9AWIN\n');
+    console.log("USAGE : node hoster.js HOST-URL");
+    process.exit(1);
+}
+
+
+
+var url = HOST;
+
+axios.get(url)
+.then((res) => {
+
+//cheerio   
+//https://axios-http.com/docs/res_schema
+
+//body
+var body = res.data;
+var statusCode = res.status;
+var statusText =  res.statusText;
+var headers = res.headers;
+var request = res.request;
+var config = res.config;
+
+//jquery
+let $ = cheerio.load(body);
+
+//variables
+console.log("/n/n");
+console.log(body);
+console.log(statusCode);
+console.log(statusText);
+console.log(headers);
+console.log(config);
+console.log(request);
+console.log("/n/n");
+
+}).catch(function (e) {
+console.log(e);
+});
